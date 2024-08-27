8e725a4fef00b0811b8902a41664b5c212cb5b86
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-08-27 20:27:46 +0000

    Install video package

--- replit.nix
+++ replit.nix
@@ -1,5 +1,15 @@
 { pkgs }: {
   deps = [
+    pkgs.haskellPackages.bindings-linux-videodev2
+    pkgs.python311Packages.get-video-properties
+    pkgs.haskellPackages.amazonka-kinesis-video-signaling
+    pkgs.python311Packages.mypy-boto3-kinesis-video-media
+    pkgs.python312Packages.types-aiobotocore-kinesis-video-archived-media
+    pkgs.mpvScripts.cutter
+    pkgs.video2midi
+    pkgs.video-trimmer
+    pkgs.octavePackages.video
+    pkgs.mpvScripts.videoclip
     pkgs.sudo
     pkgs.flutter322
     pkgs.flutter313
